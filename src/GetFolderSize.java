35a7ae628a32e8640157fc0fbc491d0be8c936d9
Yutong Jin <yjin3@scu.edu>
2018-07-21 02:15:49 -0700

    720

--- src/GetFolderSize.java
+++ src/GetFolderSize.java
@@ -9,15 +9,25 @@ public class GetFolderSize {
     int totalFolder = 0;
 
     int totalFile = 0;
-
+    int javaFile = 0;
+    int interfaceFile = 0;
 
     public static void calculate(){
         try {
 
             GetFolderSize size = new GetFolderSize();
+           // File file=new File(Main.folder);
 
+         //   String fileName=file.getName();
+//
+          //  String fileTyle=fileName.substring(fileName.lastIndexOf("."),fileName.length());
+
+            //System.out.println(//fileTyle);
+          //  if(!file.isDirectory())
+           // System.out.println(file.getName().substring(file.getName().lastIndexOf("."),file.getName().length()));
+       //     else System.out.println("dir");
              long fileSizeByte = size.getFileSize(new File(Main.folder));
-            System.out.println();
+             System.out.println();
              // System.out.println("Folder Size: " + fileSizeByte + " Bytes");
 
             System.out.println("Total Number of types: "+ size.getTotalFolder());
@@ -46,31 +56,33 @@ public class GetFolderSize {
                 totalFile++;
 
                 foldersize += filelist[i].length();
+                for (File file : filelist) {
+                    if (file.getName().substring(file.getName().lastIndexOf("."), file.getName().length()).equals("java"))
+                        javaFile++;
+                }
+
 
             }
-
+            if (!folder.getName().equals("src") && !folder.getName().equals("problems")) {
+                System.out.printf("%-20s", "Type : " + folder.getName());
+                System.out.print("  Number :" + filelist.length + "\n");
+            }
+        }
+            return foldersize;
 
         }
-        if(!folder.getName().equals("src" )&&!folder.getName().equals("problems" )) {
-            System.out.printf("%-20s", "Type : " + folder.getName());
-            System.out.print("  Number :" + filelist.length + "\n");
+
+        public int getTotalFolder () {
+
+            return totalFolder;
+
         }
 
-        return foldersize;
+        public int getTotalFile () {
+
+            return totalFile;
+
+        }
 
     }
-
-    public int getTotalFolder() {
-
-        return totalFolder;
-
-    }
-
-    public int getTotalFile() {
-
-        return totalFile;
-
-    }
-
-}
 
